71dc25dca964c19202334ce5cfa5d9d3bff5986b
Jan Weiß <jan@geheimwerk.de>
2013-10-23 16:54:56 +0200

    Implementing Demo document loading and saving.

--- Demo/Mac/Source/Document.m
+++ Demo/Mac/Source/Document.m
@@ -8,28 +8,38 @@
 
 #import "Document.h"
 
-@implementation Document
+#import <WebKit/WebKit.h>
+
+NSString * const	MarkdownDocumentType	= @"net.daringfireball.markdown";
+
+@implementation Document {
+	IBOutlet NSTextView *	_markdownTextView;
+	IBOutlet WebView *		_previewWebView;
+	
+	NSTextStorage *			_markdownText;
+}
 
 - (id)init
 {
     self = [super init];
+	
     if (self) {
-		// Add your subclass-specific initialization here.
+		_markdownText = [[NSTextStorage alloc] init];
     }
+	
     return self;
 }
 
 - (NSString *)windowNibName
 {
-	// Override returning the nib file name of the document
-	// If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
 	return @"Document";
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
 	[super windowControllerDidLoadNib:aController];
-	// Add any code here that needs to be executed once the windowController has loaded the document's window.
+	
+	[[_markdownTextView layoutManager] replaceTextStorage:_markdownText];
 }
 
 + (BOOL)autosavesInPlace
@@ -39,21 +49,39 @@
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-	// Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-	// You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-	NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-	@throw exception;
-	return nil;
+	NSData *data = nil;
+	
+	[_markdownTextView breakUndoCoalescing];
+	
+	data = [[_markdownText string] dataUsingEncoding:NSUTF8StringEncoding];
+	
+	return data;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-	// Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-	// You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-	// If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-	NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-	@throw exception;
-	return YES;
+	BOOL result;
+	
+	NSStringEncoding encoding = NSUTF8StringEncoding;
+	
+	NSString *markdownString = [[NSString alloc] initWithData:data
+													 encoding:encoding];
+	if (markdownString != nil) {
+		[_markdownText replaceCharactersInRange:NSMakeRange(0, _markdownText.length)
+									 withString:markdownString];
+		result = YES;
+	}
+	else {
+		if (outError) {
+			*outError = [NSError errorWithDomain:NSCocoaErrorDomain
+											code:NSFileReadInapplicableStringEncodingError
+										userInfo:nil];
+		}
+		
+		result = NO;
+	}
+	
+	return result;
 }
 
 @end
