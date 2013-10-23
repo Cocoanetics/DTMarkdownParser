9ad8a6a93b0d8599a84b94bc601b7931ba740e71
Oliver Drobnik <oliver@cocoanetics.com>
2013-10-23 14:58:43 +0200

    Added Demo (Mac) stub

---
+++ Demo/Mac/Source/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Demo (Mac)
+//
+//  Created by Oliver Drobnik on 23.10.13.
+//  Copyright (c) 2013 Cocoanetics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Demo/Mac/Source/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Demo (Mac)
+//
+//  Created by Oliver Drobnik on 23.10.13.
+//  Copyright (c) 2013 Cocoanetics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
